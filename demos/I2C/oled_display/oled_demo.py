7fbeb9448a30265ed0e25a6395f78f01e467ae63
ryan <ryan@smartcow.ai>
2021-12-21 08:20:39 +0000

    fixed dependency issue

--- demos/I2C/oled_display/oled_demo.py
+++ demos/I2C/oled_display/oled_demo.py
@@ -8,7 +8,7 @@ from PIL import ImageDraw
 from PIL import ImageFont
 
 # 128x32 display with hardware I2C:
-disp = Adafruit_SSD1306.SSD1306_128_32(rst=None, i2c_bus=8, gpio=1)
+disp = Adafruit_SSD1306.SSD1306_128_64(rst=None, i2c_bus=1, gpio=1)
 
 # Initialize library.
 disp.begin()
